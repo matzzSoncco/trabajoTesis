0301245211190f8fa1fe91fc5feef49184c55a1c
klismannSis <kchancuana@unsa.edu.pe>
2024-06-22 08:45:01 -0500

    Ready

--- programa/proyecto/settings.py
+++ programa/proyecto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +42,8 @@ INSTALLED_APPS = [
 ]
 #--------------------------------Se añadio media para subir el archivo-------
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
